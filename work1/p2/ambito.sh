18887494e12c90061e6c183d716f9624f0cf5b07
MendozaEmerson <emendozahi@unsa.edu.pe>
2022-06-06 22:14:36 +0000

    Adicion

---
+++ work1/p2/ambito.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo $PATH
+export PATH=$PATH:/home/emerson/PS:/home/emerson/archives/tema2:/home/emerson/archives
+echo $PATH
---
+++ work1/p3/dosArgumentos.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [$#!=2]
+then
+        printf "\nError"
+        return
+else
+
+        printf "\n Primer argumento: $1"
+        printf "\n Segundo argumento: $2 \n"
+fi
