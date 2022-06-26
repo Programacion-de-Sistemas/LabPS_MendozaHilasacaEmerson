2990e8b0c2cfb03a65cd224b4247d894cead0e1f
MendozaEmerson <emendozahi@unsa.edu.pe>
2022-06-26 17:50:12 -0500

    Se modifico la funcion que convierte a binarios

--- work3/bic_bit.c
+++ work3/bic_bit.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int binario(int n) {
+void binario(int n, int arreglo[]) {
 	int z,x,rep;
-	char value = ' ';
-	int binarys[50];
-	binarys[0] = 0;
+	char value[50];
+	arreglo[0] = 0;
 	for (x = 0; x < 50; x++) {
-		binarys[x]=n%2;
+		arreglo[x]=n%2;
 		n=n/2;
 		if(n==0)
 			break;
 	}
 	for(z = x; z >= 0 ;z--) {
-		rep = binarys[z];
-		value = value + (char)rep;
+		rep = arreglo[z];
+		value[z] = (char)rep;
+		printf("%d",arreglo[z]);
 	}
-	int valor = (int)value;
-	return valor;
+	
 }
 
-int bis(int x, int m);
+int bis(int x, int m){
+	return 0;
+}
 int bic(int x, int m);
 /* Calcular x|y usando solo llamadas a las funciones bis y bic */
 int bool_or(int x, int y) {
@@ -33,9 +34,7 @@ int bool_xor(int x, int y) {
 	return resultado;
 }
 int main() {
-	
-        char text = (char)binario(7);
-	
-	printf("7 decimal" + text);
+	int array[50];
+        binario(7, array);
 	return 0;
 }
