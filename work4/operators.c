f12305469b361b01945e925bca42d9058787b00b
MendozaEmerson <emendozahi@unsa.edu.pe>
2022-07-03 14:50:06 -0500

    Se arreglo el problema anterior y agrego otro con problema de segmentacion

--- work4/operators.c
+++ work4/operators.c
@@ -6,14 +6,26 @@
  //    *   Max ops: 14
  //     *   Rating: 1
  //     
-int bitXor(int x, int y) {
-	if(x != y)
-		return 1;	
-	return 0;
+int bitXor(int x, int y) {	
+	return (~x & y) + (x & ~y);
+}
+
+/* 
+ *  * tmin - return minimum two's complement integer 
+ *   *   Legal ops: ! ~ & ^ | + << >>
+ *    *   Max ops: 4
+ *     *   Rating: 1
+ *      */
+int tmin(void) {
+	  return ~tmin();
 }
 
 int main() {
-	int value = bitXor(4, 5);
-	printf("%d", value);
+	int value = bitXor(7, 8);
+
+	int val2 = 6;
+	val2 = tmin();
+
+	printf("%d", val2);
 	return 0;
 }
